0bf33b0cbab3c42474ced1857fd7cd8b77bebfa9
Excal-rs <excal.rs@protonmail.com>
2023-09-07 18:24:27 +0100

    finished - with comments

--- scripts/form.php
+++ scripts/form.php
@@ -4,46 +4,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $wonder = $_POST["wonder-option"];
     $story = $_POST["story"];
+    // Brings in all the data from the HTML form and then stores it in the corresponding variables
 
     $file = fopen("../database.csv", "a");
+    // Opens up a CSV file in the append mode, will create a file if it doesn't exist
 
     fputcsv($file, [$name, $email, $wonder, $story]);
+    //  Writes to teh CSV file
 
     fclose($file);
+    // Closes the file
 
     echo "Data submitted successfully!";
 }<?php
-if ($_SERVER["REQUEST_METHOD"] -- "POST") {<?php
-    if ($_SERVER["REQUEST_METHOD"] -- "POST") {
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $wonder = $_POST["wonder-option"];
-        $story = $_POST["story"];
-    
-        $file = fopen("../database.csv", "a");
-    
-        fputcsv($file, [$name, $email, $wonder, $story]);
-    
-        fclose($file);
-    
-        echo "Data submitted successfully!";
-    }
-    
-    ?>
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $wonder = $_POST["wonder-option"];
-    $story = $_POST["story"];
-
-    $file = fopen("../database.csv", "a");
-
-    fputcsv($file, [$name, $email, $wonder, $story]);
-
-    fclose($file);
-
-    echo "Data submitted successfully!";
-}
-
-?>
-
-?>
